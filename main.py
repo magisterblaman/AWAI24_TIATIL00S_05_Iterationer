6984da5b38cf8194f2b1ae098fff1a51af918c28
magisterblaman <hannes.blaman@lbs.se>
2024-09-24 10:46:00 +0200

    Grundläggande for

--- main.py
+++ main.py
@@ -1,26 +1,2 @@
-my_bool = True
-my_bool2 = False
-
-x = 4
-
-if my_bool:
-    print("Villkoret är sant")
-
-i = 1
-
-while i <= 10:
-    print(i)
-    i += 1
-
-game_over = False
-
-while not game_over:
-    print("Spel spel spel spel spel")
-    print("Spel spel spel spel spel")
-    print("Spel spel spel")
-    print("Spel spel spel spel spel spel spel")
-    answer = input("Vill du spel? ")
-    if answer != "spel":
-        game_over = True
-
-print("Inte spel")
+for i in range(15):
+    print("Hej #" + str(i))
