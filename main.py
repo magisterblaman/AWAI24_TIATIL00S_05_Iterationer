6de81091a3b6d1ec7981935070f1ea507664ab66
magisterblaman <hannes.blaman@lbs.se>
2024-09-24 10:59:46 +0200

    For loop med steg och nästlade for loops

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-for i in range(15):
+
+# Rökna baklänges
+for i in range(15, 5, -1):
     print("Hej #" + str(i))
+
+for i in range(0, 10, 2):
+    print(i)
+
+for i in range(10):
+    for j in range(10):
+        print("#", end="")
+    print("")
